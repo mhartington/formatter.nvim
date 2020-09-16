36002e4ee9b75fbd0a7795cc38cf0cd7926e70a6
Mike Hartington <mikehartington@gmail.com>
2020-09-15 20:03:23 -0400

    fix(config): properly setup config table Ref #2 Properly creating tables

--- lua/format/config.lua
+++ lua/format/config.lua
@@ -1,13 +1,13 @@
 local vim = vim
 _FormatterConfigurationValues = _FormatterConfigurationValues or {}
 
-local config = {
-  values = _FormatterConfigurationValues,
-  set_defaults = function(defaults)
-    defaults = defaults or {}
-    config.values = defaults
-  end
-}
+local config = {}
+config.values = _FormatterConfigurationValues
+
+function config.set_defaults(defaults)
+  defaults = defaults or {}
+  config.values = defaults
+end
 
 config.set_defaults()
 
