8318050cb2621ec09d0d722ed6acc64fc7d38f93
Mike Hartington <mikehartington@gmail.com>
2020-10-29 09:34:29 -0400

    chore(): cleanup

--- lua/format/formatter.lua
+++ lua/format/formatter.lua
@@ -25,7 +25,7 @@ function formatter.format(args, startLine, endLine)
   vim.fn.winrestview(view)
 end
 
-function formatter.internalFormatter(userFmtTable, startLine, endLine)
+function formatter.internalFormatter(userPassedFmt, startLine, endLine)
   local filetype = vim.fn.eval "&filetype"
   local formatters = config.values[filetype]
   -- No formatters defined for the given file type
@@ -33,59 +33,62 @@ function formatter.internalFormatter(userFmtTable, startLine, endLine)
     print(string.format("Format: no formatter defined for %s files", filetype))
     return
   end
-  if not util.isEmpty(userFmtTable) then
-    for _, userFmt in ipairs(userFmtTable) do
+
+  -- User passed a formatter as an arg
+  -- Only run this one formatter if it's valid
+  if not util.isEmpty(userPassedFmt) then
+    for _, userFmt in ipairs(userPassedFmt) do
       if not util.isEmpty(formatters[userFmt]) then
         formatter.startTask(userFmt, formatters[userFmt], startLine, endLine)
       end
     end
   else
-    for fmt, conf in pairs(formatters) do
+    print(type(formatters))
+    -- User did not pass an arg, run them all
+    for fmt, conf in pairs(formatters)
+    do
       formatter.startTask(fmt, conf, startLine, endLine)
     end
   end
 end
 
 function formatter.startTask(name, conf, startLine, endLine)
-  local o = conf()
-  local bufnr = api.nvim_get_current_buf()
-  local lines = util.getLines(bufnr, startLine, endLine)
-  local exe = o.exe
-  local exe_args = table.concat(o.args, " ")
-  local stdin = o.stdin or false
-  local output = nil
+    local o = conf()
+    local bufnr = api.nvim_get_current_buf()
+    local lines = util.getLines(bufnr, startLine, endLine)
+    local exe = o.exe
+    local exe_args = table.concat(o.args, " ")
+    local stdin = o.stdin or false
+    local output = nil
 
-  local function onStderr(_, data, _)
-    if data then
-      util.log(string.format("Format: failed to run %s", name))
+    local function onStderr(_, data, _)
+      if data then
+        util.log(string.format("Format: failed to run %s", name))
+      end
     end
-  end
-  local function onStdout(_, data, _)
-    if data then
-      output = data
+    local function onStdout(_, data, _)
+      if data then
+        output = data
+      end
     end
-  end
-  local function onExit()
-    util.setLines(bufnr, startLine, endLine, output)
-    util.log(string.format("Format: finished running %s", name))
-  end
+    local function onExit()
+      util.setLines(bufnr, startLine, endLine, output)
+      util.log(string.format("Format: finished running %s", name))
+    end
+    if stdin == true then
+      local job = runner:new({
+          cmd = exe,
+          args = exe_args,
+          on_stdout = onStdout,
+          on_stderr = onStderr,
+          on_exit = onExit
+        })
+      job.send(lines)
 
-  if stdin == true then
-    local job =
-      runner:new(
-      {
-        cmd = exe,
-        args = exe_args,
-        on_stdout = onStdout,
-        on_stderr = onStderr,
-        on_exit = onExit
-      }
-    )
-    job.send(lines)
-  else
-    print "todo: handle non-stdin"
-  end
+    else
+      print "todo: handle non-stdin"
+    end
+
 end
 
 return formatter
-
