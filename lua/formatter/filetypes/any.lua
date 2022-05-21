03494863dc4d6d080f976efcc522c339084467e4
Hrle <32d9c8c6-8118-45e7-857e-8b45522f395d@anonaddy.me>
2022-05-21 14:51:21 +0200

    remove sd and just use sed

--- lua/formatter/filetypes/any.lua
+++ lua/formatter/filetypes/any.lua
@@ -5,6 +5,4 @@ local util = require "formatter.util"
 
 M.remove_trailing_whitespace = util.withl(defaults.sed, "[ \t]*$")
 
-M.remove_trailing_whitespace_sd = util.withl(defaults.sd, "[ \t]*$")
-
 return M
