0e23ad720cb909d68d43f79bb76f47a42c98d9c1
Ruslan <kros8925@gmail.com>
2022-08-03 19:46:20 +0000

    replace _value with :get().

--- lua/formatter/filetypes/sh.lua
+++ lua/formatter/filetypes/sh.lua
@@ -1,8 +1,8 @@
 local M = {}
 
 function M.shfmt()
-  local shiftwidth = vim.opt.shiftwidth._value
-  local expandtab = vim.opt.expandtab._value
+  local shiftwidth = vim.opt.shiftwidth:get()
+  local expandtab = vim.opt.expandtab:get()
 
   if not expandtab then
     shiftwidth = 0
