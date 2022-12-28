92b3eec00d2b553b392026779740ae0451d0b406
Eleanor Bartle <eleanor@eleanor-nb.com>
2022-12-28 14:19:39 +1100

    Add --ast-check option for Zig
    `zig fmt` comes with an option to simultaneously check the AST for basic mistakes like not using a variable. This is what I use in my config and it's really nice. I have added a preset for this.

--- lua/formatter/filetypes/zig.lua
+++ lua/formatter/filetypes/zig.lua
@@ -4,5 +4,6 @@ local defaults = require "formatter.defaults"
 local util = require "formatter.util"
 
 M.zigfmt = util.copyf(defaults.zigfmt)
+M.zigfmt_astcheck = util.copyf(defaults.zigfmt_astcheck)
 
 return M
