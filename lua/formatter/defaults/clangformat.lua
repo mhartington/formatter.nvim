befc62defbf2d8ff18a340eb9432fdec5702772d
Igor Guerrero <igfgt1@gmail.com>
2023-02-26 15:07:57 -0600

    Add current buffer's indent to clang-format, as a default instead of 2 (#222)

--- lua/formatter/defaults/clangformat.lua
+++ lua/formatter/defaults/clangformat.lua
@@ -4,6 +4,7 @@ return function()
   return {
     exe = "clang-format",
     args = {
+      '-style="{IndentWidth: ' .. vim.api.nvim_buf_get_option(0, "tabstop") .. '}"',
       "-assume-filename",
       util.escape_path(util.get_current_buffer_file_name()),
     },
