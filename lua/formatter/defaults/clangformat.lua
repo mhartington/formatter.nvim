9bf38cb1977facf146a17148715ac98e220434f1
Haruka Hatori <hc.hatori@gmail.com>
2023-03-17 15:55:51 -0400

    revert clang-format args

--- lua/formatter/defaults/clangformat.lua
+++ lua/formatter/defaults/clangformat.lua
@@ -4,7 +4,6 @@ return function()
   return {
     exe = "clang-format",
     args = {
-      '-style="{IndentWidth: ' .. vim.api.nvim_buf_get_option(0, "tabstop") .. '}"',
       "-assume-filename",
       util.escape_path(util.get_current_buffer_file_name()),
     },
