f08f304f12b3ad628091c4b0ea9144e22c51f2ec
Luka Potočnik <luka.potochnik@gmail.com>
2023-07-13 13:33:21 +0200

    Add google-java-format

--- lua/formatter/filetypes/java.lua
+++ lua/formatter/filetypes/java.lua
@@ -1,9 +1,23 @@
 local M = {}
 
+local util = require "formatter.util"
+
 function M.clangformat()
     return {
         exe = "clang-format",
         args = {"--style=Google", "--assume-filename=.java"},
+        stdin = true
+    }
+end
+
+function M.google_java_format()
+    return {
+        exe = "google-java-format",
+        args = {
+            "--aosp",
+            util.escape_path(util.get_current_buffer_file_path()),
+            "--replace"
+        },
         stdin = true
     }
 end
