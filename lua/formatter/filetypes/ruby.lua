d675a567a672c850f56fd6e8513cde52eeccef09
Otávio Schwanck dos Santos <otavioschwanck@gmail.com>
2023-08-02 13:26:43 +0100

    feat: add bundle rubocop and unsafe rubocop (-A)

--- lua/formatter/filetypes/ruby.lua
+++ lua/formatter/filetypes/ruby.lua
@@ -8,7 +8,56 @@ function M.rubocop()
     args = {
       "--fix-layout",
       "--stdin",
-      util.escape_path(util.get_current_buffer_file_name()),
+      util.escape_path(util.get_current_buffer_file_path()),
+      "--format",
+      "files",
+      "--stderr",
+    },
+    stdin = true,
+  }
+end
+
+function M.rubocop_with_unsafe()
+  return {
+    exe = "rubocop",
+    args = {
+      "-A",
+      "--stdin",
+      util.escape_path(util.get_current_buffer_file_path()),
+      "--format",
+      "files",
+      "--stderr",
+    },
+    stdin = true,
+  }
+end
+
+function M.bundle_rubocop()
+  return {
+    exe = "bundle",
+    args = {
+      "exec",
+      "rubocop",
+      "--fix-layout",
+      "--stdin",
+      util.escape_path(util.get_current_buffer_file_path()),
+      "--format",
+      "files",
+      "--stderr",
+    },
+    stdin = true,
+  }
+end
+
+function M.bundle_rubocop_with_unsafe()
+  return {
+    exe = "bundle",
+    args = {
+      "exec",
+      "rubocop",
+      "-A",
+      "--stdin",
+      util.escape_path(util.get_current_buffer_file_path()),
       "--format",
       "files",
       "--stderr",
