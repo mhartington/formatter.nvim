75a96583c44962959792fa0bfc5cfbf155b05665
Marco Fernandez <marco.fernandez.99@gmail.com>
2023-10-18 16:27:19 -0700

    Add native trailing whitespace removal

--- lua/formatter/filetypes/any.lua
+++ lua/formatter/filetypes/any.lua
@@ -5,4 +5,8 @@ local util = require "formatter.util"
 
 M.remove_trailing_whitespace = util.withl(defaults.sed, "[ \t]*$")
 
+M.substitute_trailing_whitespace = function()
+    vim.cmd([[silent! :keeppatterns %s/\[ \t]+$//ge]])
+end
+
 return M
