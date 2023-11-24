ebc67e9b218c422ad0aae35296e8219379f9b60e
Denis Cornehl <denis@cornehl.org>
2023-11-24 07:58:41 +0100

    add deno fmt for markdown formatting

--- lua/formatter/filetypes/markdown.lua
+++ lua/formatter/filetypes/markdown.lua
@@ -7,4 +7,11 @@ M.prettier = util.copyf(defaults.prettier)
 
 M.prettierd = util.copyf(defaults.prettierd)
 
+function M.denofmt()
+  local denofmt = util.copyf(defaults.denofmt)()
+  table.insert(denofmt.args, "--ext")
+  table.insert(denofmt.args, "md")
+  return denofmt
+end
+
 return M
