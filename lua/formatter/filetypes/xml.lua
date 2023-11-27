9ce3e0b94b680f657327d5f89c2c5233ed9e82ca
Mike Hartington <mikehartington@gmail.com>
2023-11-27 12:16:05 -0500

    chore(): add xmllint
    Closes #247. Closes #301

--- lua/formatter/filetypes/xml.lua
+++ lua/formatter/filetypes/xml.lua
@@ -19,6 +19,17 @@ function M.tidy()
   }
 end
 
+function M.xmllint()
+  return {
+    exe = "xmllint",
+    args = {
+      "--format",
+      util.escape_path(util.get_current_buffer_file_path()),
+    },
+    stdin = true,
+  }
+end
+
 M.xmlformat = util.copyf(defaults.xmlformat)
 
 return M
