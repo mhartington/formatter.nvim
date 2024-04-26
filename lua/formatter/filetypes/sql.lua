94c2b36aed0c72c92e849a0185675b8dfaf94afa
Wahyu nurcahyo <sandun@AzizA.168.0.109>
2024-04-26 15:54:16 +0700

    add sql-formatter config

--- lua/formatter/filetypes/sql.lua
+++ lua/formatter/filetypes/sql.lua
@@ -1,21 +1,27 @@
 local M = {}
 
 function M.pgformat()
-  return {
-    exe = "pg_format --inplace -",
-    stdin = true,
-  }
+	return {
+		exe = "pg_format --inplace -",
+		stdin = true,
+	}
 end
 
 function M.sqlfluff()
-  return {
-    exe = "sqlfluff",
-    args = {
-      "format",
-      "--disable-progress-bar",
-      "--nocolor",
-      "-",
-    },
+	return {
+		exe = "sqlfluff",
+		args = {
+			"format",
+			"--disable-progress-bar",
+			"--nocolor",
+			"-",
+		},
+		stdin = true,
+		ignore_exitcode = true,
+	}
+end
+
+return M
     stdin = true,
     ignore_exitcode = true,
   }
