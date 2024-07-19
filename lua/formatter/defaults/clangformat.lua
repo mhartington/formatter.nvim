5923cd45720d9c335ed4966821dc7e831f671fa4
Root lee <irootlee@gmail.com>
2024-07-19 18:33:43 +0800

    Update clangformat.lua

--- lua/formatter/defaults/clangformat.lua
+++ lua/formatter/defaults/clangformat.lua
@@ -3,7 +3,11 @@ local util = require "formatter.util"
 return function()
   return {
     exe = "clang-format",
-    stdin = false,
+    args = {
+      "-assume-filename",
+      util.escape_path(util.get_current_buffer_file_name()),
+    },
+    stdin = true,
     try_node_modules = true,
   }
 end
