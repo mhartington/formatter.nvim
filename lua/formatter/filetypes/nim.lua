85361c7f5576556312a785c3aa64cf073765a6d4
svenrdz <svenrdz@users.noreply.github.com>
2024-09-24 15:23:19 +0200

    Add nim lang with formatters nimpretty and nph (#317)
    * add nim language with nph formatter
    
    * add nimpretty for nim lang
    
    * remove args

---
+++ lua/formatter/filetypes/nim.lua
@@ -0,0 +1,17 @@
+local M = {}
+
+function M.nimpretty()
+  return {
+    exe = "nimpretty",
+    stdin = false,
+  }
+end
+
+function M.nph()
+  return {
+    exe = "nph",
+    stdin = false,
+  }
+end
+
+return M
