0afbaea89417221643878952e4824a539f31f536
Jibin George <hello@jibingeo.com>
2024-09-24 19:00:13 +0530

    Fix heex formatter (#315)

--- lua/formatter/defaults/mixformat.lua
+++ lua/formatter/defaults/mixformat.lua
@@ -1,8 +1,12 @@
+local util = require "formatter.util"
+
 return function()
   return {
     exe = "mix",
     args = {
       "format",
+      "--stdin-filename",
+      util.escape_path(util.get_current_buffer_file_path()),
       "-",
     },
     stdin = true,
